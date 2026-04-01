2c47c64411ec78a87f7a86e5ad24a9899279c687
krjmw <111421587+krjmw@users.noreply.github.com>
2026-04-01 15:23:09 +0800

    Update deploy.sh

--- alert/deploy.sh
+++ alert/deploy.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-kubectl apply -f rabbitmq-alerts.yaml
-kubectl apply -f sock-shop-alert.yml
-kubectl label prometheusrule -n monitoring my-sock-shop-alerts release=prometheus
-kubectl label prometheusrule -n monitoring rabbitmq-alerts release=prometheus
+kubectl apply -f ./
 
